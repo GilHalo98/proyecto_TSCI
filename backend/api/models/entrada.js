16118cfcc516efa9d38600420d8a768f0f6b3577
diego <>
2021-10-25 22:08:18 -0500

    FK de reportes modificados

--- backend/api/models/entrada.js
+++ backend/api/models/entrada.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Entrada.belongsTo(models.Producto);
+      Entrada.belongsTo(models.Producto, {foreignKey: "id_producto"});
     }
   };
   Entrada.init({
