56b00544fd7c8df133ccada2ff08dbb60a28ec39
Diego Gil <diego_gilhalo98@homail.com>
2021-11-09 11:22:44 -0600

    Componente link eliminado

--- frontend/src/containers/Layout/topbar/Topbar.jsx
+++ frontend/src/containers/Layout/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import TopbarSidebarButton from './TopbarSidebarButton';
 import TopbarProfile from './TopbarProfile';
 
